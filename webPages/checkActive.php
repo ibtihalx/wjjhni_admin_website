0e38aff24676028b4dbd3a991d021e1da61da1b7
ibtihal <ebtihal_155@hotmail.com>
2024-03-18 22:13:44 +0300

    update stars display and buttons for activation are fixed

--- webPages/checkActive.php
+++ webPages/checkActive.php
@@ -4,6 +4,7 @@ session_start();
 $_SESSION['page'] = 'addStudent.php';
 if (!isset($_SESSION['logged_in'])) {
     header("Location: index.php");
+    exit();
 }
 require '../vendor/autoload.php';
 
@@ -15,12 +16,18 @@ $f->setDocumentName("activationDocument");
 if(isset($_POST['check'])){
     $document=$f->getData();
     echo $document['isActive'];
+    exit();
 }
+
+
+
+
 if(isset($_POST['update'])){
     $value= $_POST['update'];
     
     $refrence=$f->getDocment();
     $refrence->update([['path' => 'isActive', 'value' => $value]]);
     echo ($value);
+    exit();
 
 }
--- webPages/ratingView.php
+++ webPages/ratingView.php
@@ -3,21 +3,56 @@ $_SESSION['page'] = 'ratingView.php';
 if (!isset($_SESSION['logged_in'])) {
     header("Location: index.php");
 }
-        require '../vendor/autoload.php';
+require '../vendor/autoload.php';
 
-        use webPages\models\Firestore;
-        // create object of firestore 
-        $f = new Firestore();
-        $collection = $f->setCollectionName('academic_advisors');
-        //retrive all advisors documents
-        $advisors = $collection->getAlldocumentsOrdered("department");
-       $db_rating= new Firestore();
+use webPages\models\Firestore;
+// create object of firestore 
+$f = new Firestore();
+$collection = $f->setCollectionName('academic_advisors');
+//retrive all advisors documents
+$advisors = $collection->getAlldocumentsOrdered("department");
+
+
+$db_rating = new Firestore();
 $collection2 = $db_rating->setCollectionName('ratings');
 
+// create object of firestore 
+$db_rate_check = new Firestore();
+$col = $db_rate_check->setCollectionName("rating_activation");
+$db_rate_check->setDocumentName("activationDocument");
+$doc = $db_rate_check->getData();
+$isActive = $doc['isActive'];
+
+
+
 // $rates=$db_rating->getAdvisorRatingsDataByDocumentId("Rcsju1juO7TyPENB3cClsHTiNzW2");
-        // print_r(count($rates));
+// print_r(count($rates));
 
+function printStars($rating)
+{
+    $fullStars = floor($rating); // Get the number of full stars
+    $halfStar = ceil($rating) != $fullStars; // Check if there's a half star
+    $emptyStars = 5 - $fullStars - ($halfStar ? 1 : 0); // Calculate the number of empty stars
 
+    // Print full stars
+    for ($i = 0; $i < $fullStars; $i++) {
+        echo "<span class='full_star'>&#9733;</span>"; // Unicode character for a star
+    }
+
+    // Print half star if exists
+    if ($halfStar) {
+        echo "<span class='full_star'>&#9734;</span>"; // Unicode character for a half star
+    }
+
+    // Print empty stars
+    for (
+        $i = 0;
+        $i < $emptyStars;
+        $i++
+    ) {
+        echo "<span class='empty_star'>&#9734;</span>"; // Unicode character for an empty star
+    }
+}
 
 ?>
 
@@ -44,44 +79,17 @@ $collection2 = $db_rating->setCollectionName('ratings');
     </script>
     <script>
         $(document).ready(function() {
-
-
-
-
-
-            function checkStatus() {
-                var status = 1;
-                $.ajax({
-                    url: 'checkActive.php',
-                    type: 'POST',
-                    data: {
-                        check: status
-                    },
-                    success: function(response) {
-                        if (response == true) {
-                            $('#btn_active').prop("disabled", true);
-                            $('#btn_notActive').prop("disabled", false);
-                            $("#act_text").html("فترة التقييم مفعلة الآن");
-                        } else {
-                            $('#btn_notActive').prop("disabled", true);
-                            $('#btn_active').prop("disabled", false);
-                            $("#act_text").html("فترة التقييم غير مفعلة الآن");
-                        }
-                    }
-                });
-            }
-            // checkStatus();
-
             function updateStatus(status) {
 
                 $.ajax({
-                    url: 'checkActive.php',
-                    type: 'POST',
-                    data: {
-                        update: status
-                    },
-                    success: function(response) {
-                        if (response == true) {
+                        url: 'checkActive.php',
+                        type: 'POST',
+                        data: {
+                            update: status
+                        },
+                        success: function(response) {
+                            response = response.trim().toLowerCase();
+                            if (response === "true") {
                             $('#btn_active').prop("disabled", true);
                             $('#btn_notActive').prop("disabled", false);
                             $("#act_text").html("فترة التقييم مفعلة الآن");
@@ -92,14 +100,13 @@ $collection2 = $db_rating->setCollectionName('ratings');
                         }
                     }
                 });
-            }
+        }
 
-            $("#btn_active").click(function() {
-                updateStatus(true);
-            });
-            $("#btn_notActive").click(function() {
-                updateStatus(false);
-            });
+        $("#btn_active").click(function() {
+            updateStatus("true");
+        }); $("#btn_notActive").click(function() {
+            updateStatus("false");
+        });
 
 
         });
@@ -128,10 +135,22 @@ $collection2 = $db_rating->setCollectionName('ratings');
             <h1> تقييم المرشدات</h1>
             <br>
             <div id="rating_activation">
-                <button id="btn_active">تفعيل فترة التقييم</button>&nbsp; &nbsp;&nbsp;&nbsp;
-                <button id="btn_notActive">إلغاء تفعيل فترة التقييم</button>
-                <br><br>
-                <h3 id="act_text">فترة التقييم مفعلة الآن</h3>
+                <?php
+                if (strcmp($isActive, "true") == 0) {
+                    echo '<button id="btn_active" disabled>تفعيل فترة التقييم</button>&nbsp; &nbsp;&nbsp;&nbsp;';
+                    echo "   <button id='btn_notActive'>إلغاء تفعيل فترة التقييم</button>";
+                    echo " <br><br>";
+                    echo " <h3 id='act_text'>فترة التقييم مفعلة الآن</h3> ";
+                } else {
+                    echo '<button id="btn_active" >تفعيل فترة التقييم</button>&nbsp; &nbsp;&nbsp;&nbsp;';
+                    echo "   <button id='btn_notActive' disabled>إلغاء تفعيل فترة التقييم</button>";
+                    echo " <br><br>";
+                    echo " <h3 id='act_text'>فترة التقييم غير مفعلة الآن</h3> ";
+                }
+
+
+
+                ?>
             </div>
 
             <div class="continer">
@@ -145,31 +164,52 @@ $collection2 = $db_rating->setCollectionName('ratings');
                         <th> تقييم التواجد وسرعة الرد</th>
                         <th>ملاحظات الطالبات</th>
                     </tr>
-                   
-                 <?php 
-                 foreach($advisors as $adv){
+
+                    <?php
+                    foreach ($advisors as $adv) {
+                        $support_direct_rate = 0;
+                        $attent_fast_resp = 0;
+                        $rules_know = 0;
+
+
                         $rates = $db_rating->getAdvisorRatingsDataByDocumentId($adv['uid']);
-                   if(empty($rates)){
+                        if (empty($rates)) {
                             echo " <tr> <td>" . $adv['name'] . "</td>";
                             echo "<td>0</td><td>-</td><td>-</td><td>-</td>";
-                            echo "<td> لايوجد"."</td>";
-                            echo"</tr>";
-                          
-                   }
-else {
+                            echo "<td> لايوجد" . "</td>";
+                            echo "</tr>";
+                        } else {
+                            $numberOfRatings = count($rates);
                             echo " <tr> <td>" . $adv['name'] . "</td>";
-                            echo "<td>".count($rates)."</td>";
-                            echo "<td></td><td></td><td></td>";
-                            echo " <td><button class='view_notes_btn' disabled>عرض الملاحظات </button></td>";
-                            echo"</tr>";
-}
+                            echo "<td>" . $numberOfRatings . "</td>";
+                            foreach ($rates as $rate) {
+                                $support_direct_rate += $rate['support_direct_rate'];
+                                $attent_fast_resp += $rate['attent_fast_resp'];
+                                $rules_know += $rate['rules_know'];
+                            }
+                            // Calculate average ratings
+                            $support_direct_rate = $support_direct_rate / $numberOfRatings;
+                            $attent_fast_resp = $attent_fast_resp / $numberOfRatings;
+                            $rules_know = $rules_know / $numberOfRatings;
 
+                            echo "<td>";
+                            echo "<span>$support_direct_rate/5</span><br>";
+                            printStars($support_direct_rate);
+                            echo "</td>";
+                            echo "<td>";
+                            echo "<span>$attent_fast_resp/5</span><br>";
+                            printStars($attent_fast_resp);
+                            echo "</td>";
+                            echo "<td>";
+                            echo "<span>$rules_know/5</span><br>";
+                            printStars($rules_know);
+                            echo "</td>";
 
-
-                      
-
-                 }
-                 ?>
+                            echo "<td><button class='view_notes_btn'>عرض الملاحظات</button></td>";
+                            echo "</tr>";
+                        }
+                    }
+                    ?>
 
                 </table>
                 <div class="students_notes">
